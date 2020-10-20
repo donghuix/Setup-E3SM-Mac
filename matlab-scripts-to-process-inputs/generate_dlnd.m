5db2ab0c19d42f4d23515330bd98cf2f64b6a7d8
donghuix <donghui.xu@pnnl.gov>
2020-10-20 13:16:23 -0700

    Update generate_dlnd.m

--- matlab-scripts-to-process-inputs/generate_dlnd.m
+++ matlab-scripts-to-process-inputs/generate_dlnd.m
@@ -4,8 +4,12 @@ function generate_dlnd(QDRAI,QOVER,lat,lon,time,startdate,isleap,fname_out)
     
     QRUNOFF = QOVER + QDRAI; % total runoff is the sum of surface and subsurface runoff
     [nlon,nlat,nt] = size(QDRAI);
+    disp(nlon)
+    disp(nlat)
+    disp(nt)
     assert(nlon == length(lon) && nlat == length(lat) && nt == length(time));
     
+    netcdf.setDefaultFormat('NC_FORMAT_64BIT'); % To write for large file
     ncid_out = netcdf.create(fname_out,'NC_CLOBBER');
     
 % +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -41,21 +45,21 @@ function generate_dlnd(QDRAI,QOVER,lat,lon,time,startdate,isleap,fname_out)
     netcdf.putAtt(ncid_out,ivar-1,'units','mm/s');
     
     ivar = 4;
-    varid(ivar) = netcdf.defVar(ncid_out,'lat',6,[dimid(1)]); 
+    varid(ivar) = netcdf.defVar(ncid_out,'lat',6,dimid(1)); 
     netcdf.putAtt(ncid_out,ivar-1,'standard_name','latitude');
     netcdf.putAtt(ncid_out,ivar-1,'long_name','latitude');
     netcdf.putAtt(ncid_out,ivar-1,'units','degrees_north');
     netcdf.putAtt(ncid_out,ivar-1,'axis','Y');
     
     ivar = 5;
-    varid(ivar) = netcdf.defVar(ncid_out,'lon',6,[dimid(2)]); 
+    varid(ivar) = netcdf.defVar(ncid_out,'lon',6,dimid(2)); 
     netcdf.putAtt(ncid_out,ivar-1,'standard_name','longitude');
     netcdf.putAtt(ncid_out,ivar-1,'long_name','longitude');
     netcdf.putAtt(ncid_out,ivar-1,'units','degrees_east');
     netcdf.putAtt(ncid_out,ivar-1,'axis','X');
     
     ivar = 6;
-    varid(ivar) = netcdf.defVar(ncid_out,'time',6,[dimid(3)]); 
+    varid(ivar) = netcdf.defVar(ncid_out,'time',6,dimid(3)); 
     netcdf.putAtt(ncid_out,ivar-1,'standard_name','time');
     if isleap
         netcdf.putAtt(ncid_out,ivar-1,'calendar','gregorian');
@@ -64,7 +68,13 @@ function generate_dlnd(QDRAI,QOVER,lat,lon,time,startdate,isleap,fname_out)
     end
     netcdf.putAtt(ncid_out,ivar-1,'units',['days since ' startdate ' 00:00:00']);
     netcdf.putAtt(ncid_out,ivar-1,'axis','T');
+    
+    varidg = netcdf.getConstant('GLOBAL');
 
+    [~,user_name]=system('echo $USER');
+    netcdf.putAtt(ncid_out,varidg,'Created_by' ,user_name(1:end-1));
+    netcdf.putAtt(ncid_out,varidg,'Created_on' ,datestr(now,'ddd mmm dd HH:MM:SS yyyy '));
+    netcdf.endDef(ncid_out);
 % +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 %
 %                           Copy variables
