a1e24d8ceb67fc902390b48f97b67fc643a09960
donghuix <donghui.xu@pnnl.gov>
2020-10-23 12:28:44 -0700

    Update cat_elm.m

--- matlab-scripts-for-mosart/cat_elm.m
+++ matlab-scripts-for-mosart/cat_elm.m
@@ -7,14 +7,16 @@ function data = cat_elm(files,varnames)
         for j = 1 : length(varnames)
             if i == 1
                 data(1).(varnames{j}) = ncread(filename,varnames{j});
-                ncid = netcdf.open(filename,'NC_NOWRITE');
-                [dimname, dimlen] = netcdf.inqDim(ncid,1);
-                netcdf.close(ncid);
-                if strcmp(dimname,'gridcell')
-                    ndim = 1;
-                else
-                    ndim = 2;
-                end
+%                 ncid = netcdf.open(filename,'NC_NOWRITE');
+%                 [dimname, dimlen] = netcdf.inqDim(ncid,1);
+%                 netcdf.close(ncid);
+                dims = size(data(1).(varnames{j}));
+                ndim = length(dims);
+%                 if strcmp(dimname,'gridcell')
+%                     ndim = 1;
+%                 else
+%                     ndim = 2;
+%                 end
             else
                 data.(varnames{j}) = cat(ndim+1,data.(varnames{j}),ncread(filename,varnames{j}));
             end
