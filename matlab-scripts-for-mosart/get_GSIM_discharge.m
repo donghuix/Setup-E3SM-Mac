5e625af88d402643d6568072d3da7efa51499944
donghuix <donghui.xu@pnnl.gov>
2020-10-26 10:31:12 -0700

    Update get_GSIM_discharge.m

--- matlab-scripts-for-mosart/get_GSIM_discharge.m
+++ matlab-scripts-for-mosart/get_GSIM_discharge.m
@@ -5,7 +5,14 @@ function [lon,lat,Sh,yr,mo,da,mu,sd,cv] = get_GSIM_discharge(station,read_bounda
         Sh = [];
     end
     
-    if read_boundary
+    if read_boundary == 2
+        Sh = struct([]);
+        addpath('/Users/xudo627/donghui/CODE/m_map/');
+        fname = ['/Users/xudo627/DATA/GSIM_metadata/GSIM_catchments/' station];
+        M=m_shaperead(fname);
+        Sh(1).X = M.ncst(:,1);
+        Sh(2).Y = M.ncst(:,2);
+    elseif read_boundary == 1
         Sh = shaperead(['/Users/xudo627/DATA/GSIM_metadata/GSIM_catchments/' station '.shp']);
     end
     filename = fullfile('/Users/xudo627/DATA/GSIM_indices/TIMESERIES/monthly',strcat(station,'.mon'));
