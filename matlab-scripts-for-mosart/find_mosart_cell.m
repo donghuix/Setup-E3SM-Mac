2ac2a4d16cf93074b3e131be0d13736438c4527a
donghuix <donghui.xu@pnnl.gov>
2020-11-04 15:01:02 -0800

    Update find_mosart_cell.m

--- matlab-scripts-for-mosart/find_mosart_cell.m
+++ matlab-scripts-for-mosart/find_mosart_cell.m
@@ -16,6 +16,8 @@
 function [ioutlet, icontributing] = find_mosart_cell(fname,lon,lat)
     
     debug = 0;
+    searching_method = 2; % method 1: searching from each cell to see if it flows to the given outlet
+                          % method 2: searching from the outlet <- much quicker!
     
     dnID = ncread(fname,'dnID');
     ID   = ncread(fname,'ID');
@@ -33,23 +35,36 @@ function [ioutlet, icontributing] = find_mosart_cell(fname,lon,lat)
     outletg = ID(ioutlet);
     
     icontributing = [];
-    tenperc = ceil(0.1*num_of_cells);
-    
-    for i = 1 : num_of_cells
-        if mod(i,tenperc) == 0
-            fprintf(['-' num2str(i/tenperc*10) '%%']);
+    if searching_method == 1
+        tenperc = ceil(0.1*num_of_cells);
+
+        for i = 1 : num_of_cells
+            if mod(i,tenperc) == 0
+                fprintf(['-' num2str(i/tenperc*10) '%%']);
+            end
+            found = dnID(i) == ID(outletg);
+            j = i;
+            while ~found && dnID(j) ~= -9999
+                j = find(ID == dnID(j));
+                found = dnID(j) == ID(outletg);
+            end
+            if found
+                icontributing = [icontributing; i];
+            end
         end
-        found = dnID(i) == ID(outletg);
-        j = i;
-        while ~found && dnID(j) ~= -9999
-            j = find(ID == dnID(j));
-            found = dnID(j) == ID(outletg);
-        end
-        if found
-            icontributing = [icontributing; i];
+        fprintf('-100%% Done!\n');
+    elseif searching_method == 2
+        found = outletg;
+        while ~isempty(found)
+            found2 = [];
+            for i = 1 : length(found)
+                upstrm = find(dnID == found(i));
+                found2 = [found2; upstrm];
+            end
+            icontributing = [icontributing; found2];
+            found = found2;
         end
     end
-    fprintf('-100%% Done!\n');
     if debug
         figure;
         plot(longxy(icontributing),latixy(icontributing),'k+'); hold on;
