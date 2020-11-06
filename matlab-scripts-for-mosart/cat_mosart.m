e3b598aa549acef73ef952c8616da28b69a87d46
donghuix <donghui.xu@pnnl.gov>
2020-11-06 10:20:50 -0800

    Update cat_mosart.m

--- matlab-scripts-for-mosart/cat_mosart.m
+++ matlab-scripts-for-mosart/cat_mosart.m
@@ -25,22 +25,47 @@ function [data,iout] = cat_mosart(files,varnames,icontributing)
             end
         end
     else
-        for i = 1 : length(files)
-            filename = fullfile(files(i).folder,files(i).name);
-            fprintf([files(i).name '\n'])
-            for j = 1 : length(varnames)
-                if i == 1
-                    areatotal = ncread(filename,'areatotal2');
-                    iout = find(areatotal(icontributing) == max(areatotal(icontributing)));
-                    tmp = ncread(filename,varnames{j});
-                    data(1).(varnames{j}) = tmp(icontributing);
-                    ndim = 1;
-                    if length(size(tmp)) > ndim + 1
-                        error('dimension error');
+        if ~isfield(icontributing,'icontributing')
+            for i = 1 : length(files)
+                filename = fullfile(files(i).folder,files(i).name);
+                fprintf([files(i).name '\n'])
+                for j = 1 : length(varnames)
+                    if i == 1
+                        areatotal = ncread(filename,'areatotal2');
+                        iout = find(areatotal(icontributing) == max(areatotal(icontributing)));
+                        tmp = ncread(filename,varnames{j});
+                        data(1).(varnames{j}) = tmp(icontributing);
+                        ndim = 1;
+                        if length(size(tmp)) > ndim + 1
+                            error('dimension error');
+                        end
+                    else
+                        tmp = ncread(filename,varnames{j});
+                        data.(varnames{j}) = cat(ndim+1,data.(varnames{j}),tmp(icontributing));
                     end
-                else
+                end
+            end
+        else
+            for i = 1 : length(files)
+                filename = fullfile(files(i).folder,files(i).name);
+                fprintf([files(i).name '\n'])
+                for j = 1 : length(varnames)
                     tmp = ncread(filename,varnames{j});
-                    data.(varnames{j}) = cat(ndim+1,data.(varnames{j}),tmp(icontributing));
+                    for k = 1 : length(icontributing)
+                        if ~isempty(icontributing(k).icontributing)
+                        if i == 1
+                            areatotal = ncread(filename,'areatotal2');
+                            iout(k)   = find(areatotal(icontributing(k).icontributing) == max(areatotal(icontributing(k).icontributing)));
+                            data(k).(varnames{j}) = tmp(icontributing(k).icontributing);
+                            ndim = 1;
+                            if length(size(tmp)) > ndim + 1
+                                error('dimension error');
+                            end
+                        else
+                            data(k).(varnames{j}) = cat(ndim+1,data(k).(varnames{j}),tmp(icontributing(k).icontributing));
+                        end
+                        end
+                    end
                 end
             end
         end
