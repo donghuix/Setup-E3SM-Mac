9437cebde5823fe740a698f6d431fa8f2107bd51
donghuix <donghui.xu@pnnl.gov>
2020-11-05 16:42:42 -0800

    Update find_mosart_cell.m

--- matlab-scripts-for-mosart/find_mosart_cell.m
+++ matlab-scripts-for-mosart/find_mosart_cell.m
@@ -13,56 +13,80 @@
 % Author: Donghui Xu
 % Date: 08/13/2020
 % ####################################################################### %
-function [ioutlet, icontributing] = find_mosart_cell(fname,lon,lat)
-    
+function [ioutlet, icontributing] = find_mosart_cell(fname,lon,lat,target_area)
+       
     debug = 0;
     searching_method = 2; % method 1: searching from each cell to see if it flows to the given outlet
                           % method 2: searching from the outlet <- much quicker!
+    
+    if nargin == 3
+        target_area = [];
+    end
     
     dnID = ncread(fname,'dnID');
     ID   = ncread(fname,'ID');
     latixy = ncread(fname,'latixy');
     longxy = ncread(fname,'longxy');
+    area   = ncread(fname,'area');
+    
+    if target_area < nanmean(area)
+        target_area = [];
+    end
     
     [m,n] = size(dnID);
     num_of_cells = m * n;
     
     dist = pdist2([longxy(:) latixy(:)],[lon lat]);
     [B,I] = sort(dist);
-    % Assume the closest grid cell is the station point we are looking for
-    % TODO: compare to the drainage area to find the outlet with similar drainage area
-    ioutlet = I(1);
-    outletg = ID(ioutlet);
     
-    icontributing = [];
-    if searching_method == 1
-        tenperc = ceil(0.1*num_of_cells);
+    if isempty(target_area)
+        search_N = 1;
+    else
+        search_N = 10;
+    end
+    
+    for ifound = 1 : search_N
+        ioutlet = I(ifound);
+        outletg = ID(ioutlet);
 
-        for i = 1 : num_of_cells
-            if mod(i,tenperc) == 0
-                fprintf(['-' num2str(i/tenperc*10) '%%']);
+        icontributing = [];
+        if searching_method == 1
+            tenperc = ceil(0.1*num_of_cells);
+
+            for i = 1 : num_of_cells
+                if mod(i,tenperc) == 0
+                    fprintf(['-' num2str(i/tenperc*10) '%%']);
+                end
+                found = dnID(i) == ID(outletg);
+                j = i;
+                while ~found && dnID(j) ~= -9999
+                    j = find(ID == dnID(j));
+                    found = dnID(j) == ID(outletg);
+                end
+                if found
+                    icontributing = [icontributing; i];
+                end
             end
-            found = dnID(i) == ID(outletg);
-            j = i;
-            while ~found && dnID(j) ~= -9999
-                j = find(ID == dnID(j));
-                found = dnID(j) == ID(outletg);
-            end
-            if found
-                icontributing = [icontributing; i];
+            fprintf('-100%% Done!\n');
+        elseif searching_method == 2
+            found = outletg;
+            while ~isempty(found)
+                found2 = [];
+                for i = 1 : length(found)
+                    upstrm = find(dnID == found(i));
+                    found2 = [found2; upstrm];
+                end
+                icontributing = [icontributing; found2];
+                found = found2;
             end
         end
-        fprintf('-100%% Done!\n');
-    elseif searching_method == 2
-        found = outletg;
-        while ~isempty(found)
-            found2 = [];
-            for i = 1 : length(found)
-                upstrm = find(dnID == found(i));
-                found2 = [found2; upstrm];
-            end
-            icontributing = [icontributing; found2];
-            found = found2;
+        if ~isempty(target_area)
+            drainage_area = nansum(area([ioutlet; icontributing]));
+            if drainage_area/target_area > 0.5 && drainage_area/target_area < 1.5
+                fprintf(['MOSART drainage area is ' num2str(drainage_area/1e6) 'km^{2}\n']);
+                fprintf(['GSIM drainage area is ' num2str(target_area/1e6) 'km^{2}\n']);
+                break;
+            end  
         end
     end
     if debug
