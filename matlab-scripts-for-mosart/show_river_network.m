059fcc1650a32e4d80ed58f1c69a6d7aa84f2592
donghuix <donghui.xu@pnnl.gov>
2022-04-12 07:10:04 -0700

    Update show_river_network.m

--- matlab-scripts-for-mosart/show_river_network.m
+++ matlab-scripts-for-mosart/show_river_network.m
@@ -1,21 +1,26 @@
-function show_river_network(fname,col)
+function show_river_network(fname,thre,col)
     
-    if nargin < 2
+    if nargin < 3
         col = 'b-';
     end
     dnID   = ncread(fname,'dnID');
     ID     = ncread(fname,'ID');
     latixy = ncread(fname,'latixy');
     longxy = ncread(fname,'longxy');
+    areaTotal2 = ncread(fname,'areaTotal2');
+    area   = ncread(fname,'area');
+    ratio  = areaTotal2 ./ nansum(area);
+    rwid   = ncread(fname,'rwid');
+
     [m,n] = size(dnID);
     
     if m> 1 && n == 1
-        show_river_1d(dnID,ID,longxy,latixy,col)
+        show_river_1d(dnID,ID,longxy,latixy,rwid,col)
     elseif m > 1 && n > 1
-        show_river_2d(dnID,ID,longxy,latixy,col)
+        show_river_2d(dnID,ID,longxy,latixy,ratio,thre,col)
     end
     
-    function show_river_2d(dnID,ID,longxy,latixy,col)
+    function show_river_2d(dnID,ID,longxy,latixy,ratio,thre,col)
         [m,n] = size(dnID);
             for i = 1 : m
                 for j = 1 : n
@@ -23,21 +28,26 @@ function show_river_network(fname,col)
                         continue;
                     else
                         [i2,j2] = find(ID == dnID(i,j));
+                        if ratio(i,j) > thre
                         plot([longxy(i,j) longxy(i2,j2)], [latixy(i,i) latixy(i2,j2)], col,'LineWidth',2); hold on;
+                        end
                         i2 = []; j2 = [];
                     end
                 end
             end
     end
 
-    function show_river_1d(dnID,ID,longxy,latixy,col)
+    function show_river_1d(dnID,ID,longxy,latixy,rwid,col)
         m = length(dnID);
+        lw = rwid./(max(rwid) - min(rwid)) .* 3;
         for i = 1 : m
             if dnID(i) == -9999
-                continue;
+                plot(longxy(i),latixy(i),'rx'); hold on;
             else
                 i2 = find(ID == dnID(i));
-                plot([longxy(i) longxy(i2)], [latixy(i) latixy(i2)], col,'LineWidth',2); hold on;
+                if lw(i) > thre
+                plot([longxy(i) longxy(i2)], [latixy(i) latixy(i2)], col,'LineWidth',lw(i)); hold on;
+                end
                 i2 = [];
             end
         end
